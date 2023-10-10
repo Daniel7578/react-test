a8390e1ae0eb0a738733a76e513329e5f341352e
Daniel7578 <d.arangoc2@uniandes.edu.co>
2023-10-10 09:17:02 -0500

    Entrega

---
+++ src/components/__tests__/likes.test.js
@@ -0,0 +1,18 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import Like from "../likes";
+test("Defaults to 0 likes", () => {
+    render(<Like />)
+    expect(screen.getByText("Likes: 0")).toBeInTheDocument();
+    });
+test("Increment likes", () => {
+    render(<Like />);
+    fireEvent.click(screen.getByText("Like"));
+    expect(screen.getByText("Likes: 1")).toBeInTheDocument();
+    }
+);
+test("Decrement likes", () => {
+    render(<Like />);
+    fireEvent.click(screen.getByText("Dislike"));
+    expect(screen.getByText("Likes: -1")).toBeInTheDocument();
+    }
+);
